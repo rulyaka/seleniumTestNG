29472af2bf68c9656a8d26ad0349fcb5e74f60d7
CybertekSchool <github@cybertekschool.com>
2019-07-28 15:17:40 -0400

    page object model day 2 part 2

--- src/test/java/com/cybertek/pages/VehiclesPage.java
+++ src/test/java/com/cybertek/pages/VehiclesPage.java
@@ -1,7 +1,12 @@
 package com.cybertek.pages;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class VehiclesPage extends NavigationBar {
 
@@ -9,4 +14,40 @@ public class VehiclesPage extends NavigationBar {
         PageFactory.initElements(Driver.get(), this);
 
     }
+
+    @FindBy(css = "div[class='page-size pull-right form-horizontal'] button[data-toggle='dropdown']")
+    public WebElement viewPerPage;
+
+    @FindBy(css = "a[title='Filters']")
+    public WebElement filters;
+
+    @FindBy(css = "a[title='Refresh']")
+    public WebElement refresh;
+
+    @FindBy(css = "a[title='Reset']")
+    public WebElement reset;
+
+    @FindBy(css = "a[title='Grid Settings']")
+    public WebElement gridSettings;
+
+    @FindBy(css = "table.table-bordered th")
+    public List<WebElement> gridHeaders;
+
+    @FindBy(xpath = "(//div[@data-role='column-manager-table-wrapper']//input[@type='checkbox'])[1]")
+    public WebElement driverCheckbox;
+
+    public WebElement gridSettingsCheckbox(String header) {
+        String xpath = "//label[.='"+header+"']/../../td[last()]/input";
+        return Driver.get().findElement(By.xpath(xpath));
+    }
+
 }
+
+
+
+
+
+
+
+
+
